4c1f7bca04572e889fb3f08e80b8f4fb6ead3edb
AJ <joysondsouzaaj@gmail.com>
2024-02-25 13:53:53 +0530

    Feat - worked on util file

--- Backend Dev/backed/src/index.js
+++ Backend Dev/backed/src/index.js
@@ -1,9 +1,19 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./env"
-})
+    path: "./env",
+});
 
+const PORT = process.env.PORT || 8000;
 
 connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("server running on port ", PORT );
+        });
+    })
+    .catch((err) => {
+        console.error("Mongo connection failed", err);
+    });
